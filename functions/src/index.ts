6b220ba5fecbfc36dc387c4d114bee55bd5908c9
Michael Schilling <mschilling@arkid.nl>
2018-01-12 21:06:49 +0100

    Simple card refactoring

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -55,30 +55,14 @@ function planTrip(assistant) {
 
         let response = assistant.buildRichResponse().addSimpleResponse(responseText);
 
-        // const card = <any>{};
-        // card.title = `${departureTime.format('HH:mm')} - ${arrivalTime.format('HH:mm')} (${duration} minutes)`;
-        // card.description = responseText.displayText;
-        // card.imageUrl = 'https://lh3.googleusercontent.com/ZmvfZYkLd_hcas4uqPK9dZH945yMvvQg3o1wyABLl378vuaM1VHJ3kqjM6DwFNK1LzXao68_Oa8l5eyayjmzy4AxLmNjlU90lRUgdNyoItic7RAX6GAJJr22gYTWfN3-GA68xXMWQT6sminU2nzqsELJRy8kUW1-GS5bM7y93d-3ZOjXEAjAdj84vWiHub5noTHnzk15_RiN6nhvhc-qg2BO3_WOo3T2jrt8Ai_rxU9WderwgIG6gJhPDP7iQj3eefon4E4_FyRVDSN9bQfJc3QnKXRr9-fh-iNTKZHw9Db1wdPX9yZVc4-PgCITcgt3Z76SMYI56A89SVuFP7Q8YaSOvsRKBIsg1qFA2xs6kxy7vavw4lt6S6ebnJe8m2sMFOmvD1PRS8patw4bEpx80VTHYPQzi5wjhoy8tIu5RAvhpIQYxxtWDd_wNAEiLcbrDCIUBXq7o1y_HRqsHHzyx2RkfHsM5vLJo8eaRTFMpKA3tRH-Pi3XhkFU6pj-WOr151lns7c45gf3nifimrVl3Gz6kNBzdKSL_3BMRTpaKEZvmglHmmYVZtBgp3WZfaz8rA--r7nxUmQ4S8X4Orq_s4qL7xs4o1ZkMb0SU7Eo=w1920-h1080-no';
-        // card.subtitle = `from ${fromCity}`;
-        // card.buttonText = 'Go';
-        // card.buttonUrl = 'http://youtube.com'
+        let basicCard = BasicCard.fromReisplan(item).asBasicCard(assistant);
+        response = response.addBasicCard(basicCard);
 
-        let basicCard = BasicCard.fromReisplan(item)
+        let options = assistant.buildCarousel();
 
-        /*
-        // Basic Card
-        let basicCard = assistant.buildBasicCard(card.description)
-          .setTitle(card.title)
-          // .addButton(card.buttonText, card.buttonUrl)
-          .setImage(card.imageUrl, card.title)
-          .setImageDisplay('CROPPED');
-
-          if (card.subtitle) {
-            basicCard = basicCard.setSubtitle(card.subtitle);
-          }
-          */
-
-          response = response.addBasicCard(basicCard.asBasicCard(assistant));
+        for( let i=1; i < result.length; i++) {
+          const carouselOption = BasicCard.fromReisplan(result[i]).asBasicCard(assistant);
+        }
 
         return assistant.tell(response);
       } else {
