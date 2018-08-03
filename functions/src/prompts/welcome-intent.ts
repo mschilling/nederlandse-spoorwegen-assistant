cbc4625df341d6c2f27932fff7b15b6110522f14
Michael Schilling <mschilling@arkid.nl>
2018-08-03 14:55:41 +0200

    Fix translations for welcome intent

--- functions/src/prompts/welcome-intent.ts
+++ functions/src/prompts/welcome-intent.ts
@@ -1,22 +1,31 @@
+import * as i18n from 'i18n';
 import { Suggestions, SimpleResponse } from 'actions-on-google';
 
 export function welcome(conv) {
+  const suggestions: string[] = [];
   if (!conv.user.last.seen) {
     conv.ask(
       new SimpleResponse({
-        text: "Hi, I'm your personal planner for the Nederlandse Spoorwegen",
-        speech: "Hi, I'm your personal planner for the Nederlandse Spoorwegen",
+        text: i18n.__('welcome_message'),
+        speech: i18n.__('welcome_message_ssml'),
       })
     );
   } else {
     conv.ask(
       new SimpleResponse({
-        text: "Hi, I'm your personal planner for the Nederlandse Spoorwegen",
-        speech: "Hi, I'm your personal planner for the Nederlandse Spoorwegen",
+        text: i18n.__('welcome_back_message'),
+        speech: i18n.__('welcome_back_message_ssml'),
       })
     );
+
+    // suggestions.push(i18n.__('label_birthdays'));
   }
   conv.ask(
-    new Suggestions('From Utrecht To Zwolle', 'Plan my Trip', 'Departures')
+    new Suggestions(
+      ...suggestions,
+      i18n.__('label_plan_trip'),
+      i18n.__('label_departures'),
+      i18n.__('label_from_a_to_b')
+    )
   );
 }
