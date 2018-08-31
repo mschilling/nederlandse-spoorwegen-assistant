8c341d6c8ab3d9a95646c2f263e6b894346694f4
Michael Schilling <mschilling@arkid.nl>
2018-08-31 15:38:03 +0200

    Increase Functions' memory to 1Gb

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,4 +6,8 @@ process.env.DEBUG = 'actions-on-google:*';
 
 import { app } from './app';
 
-exports.assistant = functions.region('europe-west1').https.onRequest(app);
+exports.assistant = functions
+  .region('europe-west1')
+  .runWith({ memory: '1GB', timeoutSeconds: 120 })
+  .https
+  .onRequest(app);
